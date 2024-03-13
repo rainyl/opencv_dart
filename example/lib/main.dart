3e5323ea3b0defec007cf99e43aa03ed8c6839e8
rainyl <rainyliusy3@gmail.com>
2024-03-13 22:07:15 +0800

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
-
-import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:image/image.dart' as img;
+import 'dart:typed_data';
+
+import 'package:camera_universal/camera_universal.dart';
+import 'package:flutter/material.dart';
 
 import 'package:opencv_dart/opencv_dart.dart' as cv;
 
@@ -20,35 +18,49 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  cv.Mat? image;
+  var images = <Uint8List>[];
+  Uint8List? videoFrame;
+  CameraController cameraController = CameraController();
 
   @override
   void initState() {
     super.initState();
+    task();
   }
 
-  Future<ui.Image> convertImageToFlutterUi(img.Image image) async {
-    if (image.format != img.Format.uint8 || image.numChannels != 4) {
-      final cmd = img.Command()
-        ..image(image)
-        ..convert(format: img.Format.uint8, numChannels: 4);
-      final rgba8 = await cmd.getImageThread();
-      if (rgba8 != null) {
-        image = rgba8;
+  Future<void> task() async {
+    await cameraController.initializeCameras();
+    await cameraController.initializeCamera(
+      setState: setState,
+    );
+    await cameraController.activateCamera(
+      setState: setState,
+      mounted: () {
+        return mounted;
+      },
+    );
+  }
+
+  void takePicture() async {
+    var frame = cv.Mat.empty();
+    if (cameraController.is_camera_init) {
+      final im = await cameraController.action_take_picture(
+          onCameraNotInit: () {}, onCameraNotSelect: () {}, onCameraNotActive: () {});
+      if (im != null) {
+        frame = cv.imread(im.path);
       }
     }
+    if (!frame.isEmpty) {
+      setState(() {
+        videoFrame = cv.imencode(cv.ImageFormat.png.ext, frame);
+      });
+    }
+  }
 
-    ui.ImmutableBuffer buffer = await ui.ImmutableBuffer.fromUint8List(image.toUint8List());
-
-    ui.ImageDescriptor id = ui.ImageDescriptor.raw(buffer,
-        height: image.height, width: image.width, pixelFormat: ui.PixelFormat.rgba8888);
-
-    ui.Codec codec = await id.instantiateCodec(targetHeight: image.height, targetWidth: image.width);
-
-    ui.FrameInfo fi = await codec.getNextFrame();
-    ui.Image uiImage = fi.image;
-
-    return uiImage;
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
   }
 
   @override
@@ -61,35 +73,64 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Native Packages'),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Image.asset(
-                  "images/lenna.png",
-                  width: 100,
-                  height: 100,
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Camera(
+                        cameraController: cameraController,
+                        onCameraNotInit: (context) {
+                          return const SizedBox.shrink();
+                        },
+                        onCameraNotSelect: (context) {
+                          return const SizedBox.shrink();
+                        },
+                        onCameraNotActive: (context) {
+                          return const SizedBox.shrink();
+                        },
+                        onPlatformNotSupported: (context) {
+                          return const SizedBox.shrink();
+                        },
+                      ),
+                    ),
+                    Expanded(child: videoFrame == null ? Text("Empty") : Image.memory(videoFrame!))
+                  ],
                 ),
-                spacerSmall,
-                ElevatedButton(
-                    onPressed: () async {
-                      final data = await DefaultAssetBundle.of(context).load("images/lenna.png");
-                      setState(() {
-                        image = cv.imdecode(data.buffer.asUint8List(), cv.IMREAD_GRAYSCALE);
-                      });
-                    },
-                    child: const Text("Process")),
-                image == null
-                    ? Text("Image is null!")
-                    : Image.memory(
-                        cv.imencode(cv.ImageFormat.png.ext, image!),
-                        width: 100,
-                        height: 100,
-                      )
-              ],
-            ),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final data = await DefaultAssetBundle.of(context).load("images/lenna.png");
+                  final im = cv.imdecode(data.buffer.asUint8List(), cv.IMREAD_COLOR);
+                  final gray = cv.Mat.empty();
+                  cv.cvtColor(im, gray, cv.COLOR_BGR2GRAY);
+                  final blur = cv.Mat.empty();
+                  cv.gaussianBlur(im, blur, (7, 7), 2, sigmaY: 2);
+                  setState(() {
+                    images = [
+                      data.buffer.asUint8List(),
+                      cv.imencode(cv.ImageFormat.png.ext, gray),
+                      cv.imencode(cv.ImageFormat.png.ext, blur)
+                    ];
+                  });
+                  takePicture();
+                },
+                child: const Text("Process"),
+              ),
+              Expanded(
+                flex: 2,
+                child: ListView.builder(
+                  itemCount: images.length,
+                  itemBuilder: (ctx, idx) => Card(
+                    child: Image.memory(images[idx]),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
