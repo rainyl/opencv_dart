e7569e774b78d01ad010e8bec08da09fe1713205
rainy liu <rainyliusy3@gmail.com>
2024-10-29 17:10:38 +0800

    hide highgui in opencv_core, opencv_dart, remove submodule

--- packages/opencv_dart/lib/opencv.dart
+++ packages/opencv_dart/lib/opencv.dart
@@ -5,4 +5,16 @@
 /// OpenCV bindings for Flutter
 library cv;
 
-export 'package:dartcv4/dartcv.dart';
+export 'package:dartcv4/calib3d.dart';
+export 'package:dartcv4/contrib.dart';
+export 'package:dartcv4/core.dart';
+export 'package:dartcv4/dnn.dart';
+export 'package:dartcv4/features2d.dart';
+export 'package:dartcv4/imgcodecs.dart';
+export 'package:dartcv4/imgproc.dart';
+export 'package:dartcv4/objdetect.dart';
+export 'package:dartcv4/photo.dart';
+export "package:dartcv4/src/g/constants.g.dart";
+export 'package:dartcv4/stitching.dart';
+export 'package:dartcv4/video.dart';
+export 'package:dartcv4/videoio.dart';
