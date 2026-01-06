3efac3e288e31becc438d31ffb030c5fd68680aa
rainy liu <rainyliusy3@gmail.com>
2026-01-06 17:37:42 +0800

    remove windows generator, use Generator.defaultGenerator (#413)

--- packages/dartcv/lib/src/hook_helpers/run_build.dart
+++ packages/dartcv/lib/src/hook_helpers/run_build.dart
@@ -9,7 +9,6 @@ import 'package:code_assets/code_assets.dart';
 import 'package:hooks/hooks.dart';
 import 'package:logging/logging.dart';
 import 'package:native_toolchain_cmake/native_toolchain_cmake.dart';
-import 'package:native_toolchain_cmake/src/native_toolchain/msvc.dart';
 
 import 'patchelf_linux.dart';
 
@@ -98,30 +97,10 @@ Future<void> runBuild(BuildInput input, BuildOutputBuilder output, {Set<String>?
     'DARTCV_WITH_XOBJDETECT': modules.contains('xobjdetect') ? 'ON' : 'OFF',
   };
 
-  var winGenerator = Generator.defaultGenerator;
-  if (input.config.code.targetOS == OS.windows) {
-    final tools = await visualStudio.defaultResolver?.resolve(logger: logger);
-    if (tools == null) {
-      throw StateError('Failed to resolve Visual Studio tools');
-    }
-    for (final tool in tools) {
-      if (tool.version?.major == 16) {
-        winGenerator = Generator.vs2019;
-      } else if (tool.version?.major == 17) {
-        winGenerator = Generator.vs2022;
-      } else {
-        logger.warning(
-          'Skip Visual Studio version: ${tool.version}, '
-          'Currently on Visual Studio 2019 or 2022 is supported.',
-        );
-      }
-    }
-  }
-
   final generator = switch (targetOS) {
     OS.linux => Generator.make,
     OS.macOS || OS.iOS => Generator.xcode,
-    OS.windows => winGenerator,
+    OS.windows => Generator.defaultGenerator,
     OS.android => Generator.ninja,
     _ => throw ArgumentError.value(targetOS, 'targetOS', 'Unsupported target OS'),
   };
