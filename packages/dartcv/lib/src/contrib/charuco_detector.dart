340d1270b55e8999d9a0456af7745bcd400d06a5
rainy liu <rainyliusy3@gmail.com>
2026-08-17 13:15:38 +0800

    update authorship

--- packages/dartcv/lib/src/contrib/charuco_detector.dart
+++ packages/dartcv/lib/src/contrib/charuco_detector.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, rainyl and all contributors. All rights reserved.
+// Copyright (c) 2024, pariterre, rainyl and all contributors. All rights reserved.
 // Use of this source code is governed by a Apache-2.0 license
 // that can be found in the LICENSE file.
 
